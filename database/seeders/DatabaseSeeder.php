a3b4437601053df8a68f21e44519373ace9dd05d
mahereldesoky <maherfared@gmail.com>
2024-01-29 11:58:46 +0200

    dsdsd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(
             [
-                OrderSeeder::class,
+                // OrderSeeder::class,
           
                 PermissionsSeeder::class
             ]
