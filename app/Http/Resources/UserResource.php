587222a984f9dba5685ad6a63dc923833756c8bf
mahereldesoky <maherfared@gmail.com>
2024-01-29 13:12:23 +0200

    dfcxv

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,10 +20,10 @@ class UserResource extends JsonResource
             "name" => $this->name,
             "email" => $this->email,
             "token" => $this->createToken('Token')->plainTextToken,
-            // "role" => $this->roles,
-            // "permissions" => $this->Permissions,
-            // "created_at" => $this->created_at,
-            // "updated_at" => $this->updated_at,
+            "role" => $this->roles,
+            "permissions" => $this->Permissions,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
         ];
     }
 }
